18ea308876807f5c19fb15013fdf19e909a29d0d
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-06-03 16:51:56 +0200

    Initial commit

---
+++ src/main/java/dev/ivanvoloshyn/redriveapi/ReDriveApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ivanvoloshyn.redriveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReDriveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReDriveApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ivanvoloshyn/redriveapi/ReDriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ivanvoloshyn.redriveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReDriveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
