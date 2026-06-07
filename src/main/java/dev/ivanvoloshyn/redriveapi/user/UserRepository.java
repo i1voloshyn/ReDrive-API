d8c676c2bdf67252de53db93f1a8ba7ee6634c2f
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-06-07 09:22:08 +0200

    Replace in-memory user repository with JPA Repository

--- src/main/java/dev/ivanvoloshyn/redriveapi/user/UserRepository.java
+++ src/main/java/dev/ivanvoloshyn/redriveapi/user/UserRepository.java
@@ -1,13 +1,9 @@
 package dev.ivanvoloshyn.redriveapi.user;
 
 import dev.ivanvoloshyn.redriveapi.user.model.User;
-import dev.ivanvoloshyn.redriveapi.user.model.UserResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
-
-    User save(User user);
 }
--- src/main/java/dev/ivanvoloshyn/redriveapi/user/model/User.java
+++ src/main/java/dev/ivanvoloshyn/redriveapi/user/model/User.java
@@ -1,5 +1,6 @@
 package dev.ivanvoloshyn.redriveapi.user.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,18 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(name = "first_name",nullable = false)
     private String firstName;
+    @Column(name = "last_name",nullable = false)
     private String lastName;
+    @Column(name = "password_hash",nullable = false)
     private String passwordHash;
 }
