d19a08bfa980eac9af2d128f1c93c8b52e9a3851
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-06-24 18:02:00 +0200

    remove unnecessary imports'

--- src/main/java/dev/ivanvoloshyn/redriveapi/exception/model/ErrorResponse.java
+++ src/main/java/dev/ivanvoloshyn/redriveapi/exception/model/ErrorResponse.java
@@ -3,7 +3,6 @@ package dev.ivanvoloshyn.redriveapi.exception.model;
 import org.springframework.http.HttpStatus;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 public record ErrorResponse(
